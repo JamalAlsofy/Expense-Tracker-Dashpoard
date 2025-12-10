1cd0bd48a17c5cc6b7ee258c059d23d2e42eb18f
jamal Alsofy <jamal.alsoofy2030@gmail.com>
2025-12-10 17:31:09 +0300

    update

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,21 +1,18 @@
 import { Component ,signal} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
-
-
 @Component({
 selector: 'app-topbar',
   standalone: false,
-
 templateUrl: './topbar.component.html',
 styleUrls: ['./topbar.component.scss'],
 })
+  
 export class TopbarComponent {
+
 isDark = signal(false);
 currentLang = signal('en');
-
-
+  
 toggleTheme() {
 this.isDark.set(!this.isDark());
 document.body.classList.toggle('dark-theme', this.isDark());
@@ -25,5 +22,7 @@ document.body.classList.toggle('dark-theme', this.isDark());
 changeLang() {
 this.currentLang.set(this.currentLang() === 'en' ? 'ar' : 'en');
 document.documentElement.lang = this.currentLang();
-}
+  }
+  
+
 }
